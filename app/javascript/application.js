cbd5d1f4b6dc493b8c727beffc2bc41ef97ba9bb
Sergey <SheyzNeh@mail.ru>
2023-04-10 17:37:49 +0300

    fix 3

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,1 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "./controllers"
-//= require jquerry
-//= require jquerry_ujs
+import * as bootstrap from "bootstrap"
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,9 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "controllers/application"
+import { application } from "./application"
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 //import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
