68c0d62f343869e01e2e076a017c109297ea3b59
Sergey <SheyzNeh@mail.ru>
2023-04-13 15:44:42 +0300

    test commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,10 @@
 import * as bootstrap from "bootstrap"
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
